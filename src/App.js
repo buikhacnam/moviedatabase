46d46f3256daf3b26e7b7d08ba61692dabaf9f3f
Bui Nam <buikhacnam11@gmail.com>
2020-11-18 21:37:10 +0700

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./components/Search";
 import Results from "./components/Results";
 import Popup from "./components/Popup";
+
 import axios from 'axios'
 
 function App() {
   const apiurl = "https://www.omdbapi.com/?apikey=1dfa35e4";
+  const starWar = "https://www.omdbapi.com/?apikey=1dfa35e4&s=star wars";
   const [state, setState] = useState({
     s: "",
     results: [],
-    selected: {}
+    resultsFirst: [],
+    loading: true,
+    selected: {},
+    nothing: false
   })
 
+  useEffect(() => {
+    axios(starWar)
+    .then(({data}) => {
+      let results = data.Search;
+      console.log(results);
+      setState(prevState => {
+        return {  
+              prevState,         
+             results
+          }
+       })
+    })
+  }, [])
+
+  //console.log(state);
+
   const search = (e) => {
-    if (e.key === "Enter") {
+    if (e.key === "Enter" && (state.s)) {
       axios(apiurl + "&s=" + state.s)
       .then((data) => {
         let initialResult = data;
@@ -23,28 +44,30 @@ function App() {
         }
 
        
-        console.log(initialResult);
+        console.log(results);
         console.log(typeof(initialResult));
         setState(prevState => {
           return {
             ...prevState,
+            loading: false,
             results
           }
         })
       });
-    }
+    } 
   }
 
   const handleInput = (e) => {
     let s = e.target.value;
-
+    if (s) {
     setState(prevState => {
       return { 
         ...prevState, 
+        
         s
       }
     })
-
+  }
     //console.log(state.s)
   }
 
@@ -52,24 +75,28 @@ function App() {
     axios(apiurl + "&i=" + id).then(({data}) => {
       let result = data;
       console.log(result);
-      
+      if (result) {
       setState(prevState => {
           return {
             ...prevState,
             selected: result
           }
       })
+      }
     })
+  
   }
 
   const closePopup = () => {
     setState(prevState => {
       return {
         ...prevState,
-        selected: {}
+        selected: false
       }
     })
   }
+
+  console.log(typeof (state.selected))
 
   return (
     <div className="App">
@@ -77,13 +104,25 @@ function App() {
        <h1>Movie Database</h1>
       </header>
       <main>
-          <Search handleInput={handleInput} search={search}/>
-          <Results results={state.results} openPopup={openPopup}/>
+          <Search handleInput={handleInput} search={search} />
+         
+         { (state.results)?
+            <Results results={state.results} openPopup={openPopup}/> :
+            <div className="no-result">
+              <h1>no result</h1>
+            </div>
 
-          {(typeof state.selected.Title !== "undefined") ?
-            <Popup selected={state.selected} closePopup={closePopup} /> :
-            false
+         }
+           
+        
+          
+          {(state.selected) &&
+            <Popup selected={state.selected} closePopup={closePopup} /> 
+            
           }
+           
+            
+        
 
       </main>
     </div>
@@ -91,3 +130,13 @@ function App() {
 }
 
 export default App;
+
+/*
+ <Results results={state.results} openPopup={openPopup}/> 
+
+   {(typeof state.selected.Title !== "undefined") ?
+            <Popup selected={state.selected} closePopup={closePopup} /> :
+            false
+          }
+
+*/
---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      <h4>data is loading....</h4>
+    
+    </div>
+  );
+};
+
+export default Loading;
